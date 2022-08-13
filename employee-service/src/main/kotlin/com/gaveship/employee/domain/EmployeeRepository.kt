ff9c310deb4af45b56921a0ca667e8d264c35c35
공준배 <ballgaveship@gmail.com>
2022-08-13 16:41:49 +0900

    chore(employee-service): repository의 패키지 이동

--- employee-service/src/main/kotlin/com/gaveship/employee/domain/EmployeeRepository.kt
+++ employee-service/src/main/kotlin/com/gaveship/employee/domain/EmployeeRepository.kt
@@ -1,5 +1,6 @@
-package com.gaveship.employee.domain.model
+package com.gaveship.employee.domain
 
+import com.gaveship.employee.domain.model.Employee
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
