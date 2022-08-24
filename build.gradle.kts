101474e5ad332492ed26b9be7ea288ee107dc13c
공준배 <ballgaveship@gmail.com>
2022-08-24 22:04:55 +0900

    chore: 디펜던시 관리 정보 개선, JVM 11 Downgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "com.gaveship"
-version = "0.0.1-SNAPSHOT"
-
-apply(from = "$rootDir/versions.gradle.kts")
-val springCloudDependenciesVersion: String by extra
-val kotlinLoggingVersion: String by extra
-val awsSpringCloudVersion: String by extra
-val hibernateTypesVersion: String by extra
-val jacksonAnnotationsVersion: String by extra
-
 plugins {
-    id("org.springframework.boot") version "2.7.2"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id ("org.jetbrains.kotlin.plugin.allopen") version "1.7.10"
-    id ("org.jetbrains.kotlin.plugin.noarg") version "1.7.10"
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.spring") version "1.7.10"
-    kotlin("plugin.jpa") version "1.7.10"
-    java
+    id("org.springframework.boot") version Versions.springBoot
+    id("io.spring.dependency-management") version Versions.springDependencyManagement
+    id ("org.jetbrains.kotlin.plugin.allopen") version Versions.kotlin
+    id ("org.jetbrains.kotlin.plugin.noarg") version Versions.kotlin
+    kotlin("jvm") version Versions.kotlin
+    kotlin("plugin.spring") version Versions.kotlin
+    kotlin("plugin.jpa") version Versions.kotlin
 }
 
 noArg {
@@ -32,14 +21,17 @@ allOpen {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 allprojects {
     repositories {
         mavenCentral()
     }
+
+    group = "com.gaveship"
+    version = "0.0.1-SNAPSHOT"
 }
 
 subprojects {
@@ -50,20 +42,20 @@ subprojects {
     dependencyManagement {
         dependencies {
             imports {
-                mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudDependenciesVersion")
+                mavenBom("org.springframework.cloud:spring-cloud-dependencies:${Versions.springCloudDependencies}")
             }
-            dependency("com.vladmihalcea:hibernate-types-52:$hibernateTypesVersion")
-            dependency("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
-            dependency("io.awspring.cloud:spring-cloud-aws-messaging:$awsSpringCloudVersion")
-            dependency("io.awspring.cloud:spring-cloud-aws-autoconfigure:$awsSpringCloudVersion")
-            dependency("com.fasterxml.jackson.core:jackson-annotations:$jacksonAnnotationsVersion")
+            dependency("com.vladmihalcea:hibernate-types-52:${Versions.hibernateTypes}")
+            dependency("io.github.microutils:kotlin-logging:${Versions.kotlinLogging}")
+            dependency("io.awspring.cloud:spring-cloud-aws-messaging:${Versions.awsSpringCloud}")
+            dependency("io.awspring.cloud:spring-cloud-aws-autoconfigure:${Versions.awsSpringCloud}")
+            dependency("com.fasterxml.jackson.core:jackson-annotations:${Versions.jacksonAnnotations}")
         }
     }
 
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "17"
+            jvmTarget = "11"
         }
     }
 }
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,10 @@
+object Versions {
+    const val kotlin = "1.7.10"
+    const val springDependencyManagement = "1.0.11.RELEASE"
+    const val springBoot = "2.7.2"
+    const val springCloudDependencies = "2021.0.1"
+    const val kotlinLogging = "1.12.5"
+    const val awsSpringCloud = "2.4.2"
+    const val hibernateTypes = "2.11.1"
+    const val jacksonAnnotations = "2.13.3"
+}
